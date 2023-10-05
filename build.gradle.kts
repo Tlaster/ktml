84dd16d15b6b094c9d387e82c9ba7c6c00e8f9ba
Tlaster <tlaster@outlook.com>
2023-10-05 14:45:32 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.7.10"
+    kotlin("multiplatform") version "1.9.0"
 }
 
 group = "moe.tlaster"
@@ -10,7 +10,6 @@ repositories {
 }
 
 kotlin {
-    ios()
     macosX64()
     macosArm64()
     jvm {
@@ -22,19 +21,27 @@ kotlin {
             useJUnitPlatform()
         }
     }
-    js {
-        browser()
-        nodejs()
-    }
-    
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+    linuxArm64()
+    linuxX64()
+    watchosArm32()
+    watchosArm64()
+    watchosDeviceArm64()
+    watchosSimulatorArm64()
+    tvosArm64()
+    tvosSimulatorArm64()
+
+
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
-                implementation("io.ktor:ktor-client-core:2.1.0")
-                implementation("io.ktor:ktor-client-okhttp:2.1.0")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+                implementation("io.ktor:ktor-client-core:2.3.4")
+                implementation("io.ktor:ktor-client-okhttp:2.3.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
             }
         }
     }
