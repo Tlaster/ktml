75ee6558541225fc75eb16df17b9b189c2797fb4
Tlaster <tlaster@outlook.com>
2023-10-05 17:00:31 +0900

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "moe.tlaster"
-version = "0.0.1"
+version = "0.0.2"
 
 repositories {
     mavenCentral()
@@ -39,6 +39,68 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+// https://github.com/gradle/gradle/issues/26091
+val signingTasks = tasks.withType<Sign>()
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    dependsOn(signingTasks)
+}
+publishing {
+    if (rootProject.file("publish.properties").exists()) {
+        signing {
+            sign(publishing.publications)
+        }
+        repositories {
+            maven {
+                val releasesRepoUrl =
+                    "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                val snapshotsRepoUrl =
+                    "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+                url = if (version.toString().endsWith("SNAPSHOT")) {
+                    uri(snapshotsRepoUrl)
+                } else {
+                    uri(releasesRepoUrl)
+                }
+                credentials {
+                    username = project.ext.get("ossrhUsername").toString()
+                    password = project.ext.get("ossrhPassword").toString()
+                }
+            }
+        }
+    }
+
+    publications.withType<MavenPublication> {
+        artifact(javadocJar)
+        pom {
+            name.set("ktml")
+            description.set("Html parser for Kotlin Multiplatform")
+            url.set("https://github.com/Tlaster/ktml")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                    id.set("Tlaster")
+                    name.set("James Tlaster")
+                    email.set("tlaster@outlook.com")
+                }
+            }
+            scm {
+                url.set("https://github.com/Tlaster/ktml")
+                connection.set("scm:git:git://github.com/Tlaster/ktml.git")
+                developerConnection.set("scm:git:git://github.com/Tlaster/ktml.git")
             }
         }
     }
